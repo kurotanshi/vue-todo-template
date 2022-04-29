38219be3729aa65e49bd310142b89332127e5761
Kuro Hsu <kurotanshi@gmail.com>
2022-04-29 12:22:11 +0800

    user login / logout

--- src/apis.js
+++ src/apis.js
@@ -1,6 +1,71 @@
 import request from './request';
 
+// 註冊
+const userReg = ({ email, nickname, password }) => {
+  return request({
+    method: 'post',
+    url: `/users`,
+    data: {
+      user: {
+        email,
+        nickname,
+        password
+      },
+    },
+  });
+};
 
-export default{ 
+// 登入
+const userLogin = ({ email, password }) => {
+  return request({
+    method: 'post',
+    url: `/users/sign_in`,
+    data: {
+      user: {
+        email,
+        password
+      },
+    },
+  });
+};
 
+// 登出
+const userLogout = () => {
+  return request({
+    method: 'delete',
+    url: `/users/sign_out`,
+  });
+};
+
+// 認證 check
+const authCheck = () => {
+  return request({
+    method: 'get',
+    url: `/check`,
+  });
+};
+
+// ======== 以下待完成 ========
+
+// 取得 TODO 列表
+
+
+// 新增 TODO
+
+
+// 修改 TODO
+
+
+// 刪除 TODO
+
+
+// 完成狀態切換
+
+
+
+export default {  
+  userReg,
+  userLogin,
+  userLogout,
+  authCheck,
 }
