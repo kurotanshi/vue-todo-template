ef33cc4700e8bef3d2c7e48023c290b5d315e40f
Kuro Hsu <kurotanshi@gmail.com>
2022-09-03 18:36:43 +0800

    1. update package 2. add pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
 import App from './App.vue';
 
 import './style.css';
 import './index.css';
 
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app')
